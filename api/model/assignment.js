c2029377e7dcbf3926a2c35806d2714ed97ce32e
Saillant <saillantnicolas@mbpdenisaillant.home>
2024-01-27 14:11:20 +0100

    Add api for render.com

---
+++ api/model/assignment.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    rendu: Boolean,
+    id: Number,
+    daterendu: Date,
+    matiere: String,
+    nom: String,
+    note: Number,
+    prof: Number,
+});
+
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('Assignment', AssignmentSchema);
